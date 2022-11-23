65e3c83faf7b3c513c596b0facdffbdefee8c399
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2022-11-22 21:37:51 -0800

    Init Commit for Chat Server Client

---
+++ src/main/java/Client.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Rawrg
+ */
+public class Client {
+    private final String HOST;
+    private final int PORT;
+    private javax.swing.JTextArea jWindow;
+    private BufferedReader in;
+    private Socket socket;
+    
+    public Client(String host, int port, javax.swing.JTextArea jWindow){
+        HOST = host;
+        PORT = port;
+        this.jWindow = jWindow;
+        
+    }
+    
+    public void readChat(Socket socket){
+        try{in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while(true){
+            try {
+                String messages = "";
+                boolean holder = false;
+                Thread.sleep(100);
+                while(in.ready()){
+                    messages += in.readLine() + "\n";
+                    holder = true;
+                }
+                if(holder){
+                    jWindow.setText(messages);
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+    }
+    public void connect(){
+        try{
+            socket = new Socket(HOST, PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            /**Do some magic shit here*/
+            Runnable read = () -> this.readChat(socket);
+            Thread t = new Thread(read);
+            t.start();
+
+        } catch (UnknownHostException e){
+            System.err.println(e);
+            System.exit(-1);
+        } catch (IOException e) {
+            System.err.println(e);
+            System.exit(-2);
+        } catch (SecurityException e) {
+            System.err.println(e);
+            System.exit(-3);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e);
+            System.exit(-4);
+        }
+        System.out.println("Fell out of scope");
+    }
+}
